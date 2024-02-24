98016369b09cc3c4829aaef3d7866c4d824b2161
Hero Molina Sanchez <heromolina@gmail.com>
2024-02-24 17:48:24 -0600

    Inicio commit de cursos CA y DI subir a github

--- DI/MOD_01/Ej0101/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ DI/MOD_01/Ej0101/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b1a7a362481313166e0a8db8ff27f5f436e20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
